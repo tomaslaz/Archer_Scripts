e8e9309837c04cd91785835875a704ab5bedaa47
Tomaslaz <tomas.lazauskas@gmail.com>
2017-06-01 17:37:41 +0100

    Adding new jobs submission script

--- JobArray.bash
+++ JobArray.bash
@@ -1,12 +1,10 @@
-#!/bin/bash --login
-
-# PBS job options (name, compute nodes, job time)
-#PBS -N TomasJobArray
-#PBS -l select=1
-#PBS -l walltime=00:01:00
-
-# Replace [project code] below with your project code (e.g. t01)
-#PBS -A 
+#!/bin/bash
+#PBS -N TiJobArray
+#PBS -l select=8
+#PBS -l walltime=00:20:00
+#PBS -A e05-gener-smw
+#PBS -J 1-10
+#PBS -r y 
 
 # load modules
 module load aims
@@ -23,68 +21,17 @@ basedir=$PBS_O_WORKDIR
 export NPROC=`qstat -f $PBS_JOBID | awk '/Resource_List.ncpus/ {print $3}'`
 
 # Set the number of threads to 1
-#   This prevents any system libraries from automatically
-#   using threading.
 export OMP_NUM_THREADS=1
 
-# Define a xyz2geometry function
-xyz2geometry() {
-  fileName="$1"
-  numLines=`wc -l $fileName | awk '{print $1}'`
-  let "numLines = $numLines - 2"
-  tail -$numLines $fileName > tmp
-  awk '{print "atom", $2, $3, $4, $1}' tmp > geometry.in
-  rm -f tmp
-}
+STRIDE=10
 
-# preparing directories for the simulations
-rm -rf output_backup
-mv -f output output_backup > /dev/null
-mkdir output > /dev/null
-
-cd input
-
-for entry in `ls $search_dir`; do
-	if [ ${entry: -4} == ".xyz" ]
-	then
-	  filename="${entry%.*}"
-	  
-	  xyz2geometry $entry
-	  
-	  echo "Preparing" $filename
-	  
-	  mkdir ../output/$filename
-	  cp -f control.in ../output/$filename/ > /dev/null
-	  mv geometry.in ../output/$filename/ > /dev/null
-	fi
+fromNo=($PBS_ARRAY_INDEX-1)*STRIDE+1
+toNo=$PBS_ARRAY_INDEX*STRIDE+1
+for (( i=$fromNo; i<$toNo; i++ ))
+do
+	workDir=`sed -n "$i"p < jobs.txt`
+	echo $workDir
+	cd $workDir
+	aprun -n 96 aims > FHIaims.out 
+	cd ..
 done
-
-cd ..
-
-echo "--------------------------"
-echo "Done preparing"
-echo "--------------------------"
-echo 
-echo
-echo "--------------------------"
-echo "Executing"
-echo "--------------------------"
-
-cd output
-
-# execute the jobs
-for entry in `ls $search_dir`; do
-	if [ -d "$entry" ] ; then
-	  cd $entry
-	  echo $entry
-	  aprun -n 24 aims > FHIaims${i}.out &
-	  
-	  cd ..
-	fi
-done
-
-cd ..
-
-# Wait for all jobs to finish before exiting the job submission script
-wait
-exit 0
